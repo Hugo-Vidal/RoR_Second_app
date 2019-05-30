d0580220f401f17fe6f82ae64eb4c342ada916c9
Hugo-Vidal <hugo.f.vidal.r@gmail.com>
2019-05-29 20:30:28 -0400

    migration add email, views, form, strong params

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_29_234000) do
+ActiveRecord::Schema.define(version: 2019_05_30_001657) do
 
   create_table "students", force: :cascade do |t|
     t.string "name"
     t.integer "age"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+
   end
 
 end
